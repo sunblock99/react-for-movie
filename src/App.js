27f118c98a44f19977680ca3bf06bd1a02e151e3
Sunblock <sunblock99@kakao.com>
2023-02-23 20:46:58 +0900

    useEffect 학습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect( () => {
+    console.log("CALL THE API.");
+  }, [])
   return (
   <div>
-    <h1 className={styles.title}>welcome back!! </h1>
-    <Button text={"Continue"} />
+    <h1>{counter}</h1>
+    <button onClick={onClick} >click me</button>
   </div>
   );
 }
