b1780f99e73074590c3c909e219e18879ec83013
Sunblock <sunblock99@kakao.com>
2023-02-27 21:00:37 +0900

    useEffect 응용해보기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => {setKeyword(event.target.value)}
-  console.log("i run all the time");
-  useEffect( () => {
-    console.log("한번만 호출돼요 ! ");
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
   }, []);
-  useEffect( () => {
-    console.log("키워드 변화 생김 !");
-  }, [keyword]);
-  useEffect(()=>{
-    console.log("카운트 변화 생김 !");
-  },[counter]);
-  useEffect(()=>{
-    console.log("키워드 와 카운트 변화 생김 !");
-  },[keyword,counter]);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-  <div>
-  <input value={keyword} type="text" onChange={onChange} placeholder="검색어를 입력해주세요. "/>
-    <h1>{counter}</h1>
-    <button onClick={onClick}>click me</button>
-  </div>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
   );
 }
 
