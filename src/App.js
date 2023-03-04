bf832a02681b94778d7f78b3a281ecc814c17105
sunblock99 <sunblock99@kakao.com>
2023-03-04 09:07:13 +0900

    todo list 만들기 - 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(todo === ""){
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [todo, ...currentArray]);
+  } 
+  console.log(todos);
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  <div>
+    <h1>나의 todo 수는  : ({todos.length})</h1>
+    <form onSubmit={onSubmit}>
+    <input onChange={onChange} value={todo} type="text" placeholder="Write your to do...." />
+    <button>Add todo</button>
+    </form>
+  </div>);
+  
 }
 
 export default App;
