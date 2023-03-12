4621579f6cec56f2892aae575fd4de73eef59086
sunblock99 <sunblock99@kakao.com>
2023-03-12 21:04:12 +0900

    home.js 수정본

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ function Home() {
   const getMovies = async () => {
     const json = await (
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=most`
       )
     ).json();
     setMovies(json.data.movies);
