78cad1c44c09e660943ae4e8bffccb480fbc8ac8
sunblock99 <sunblock99@kakao.com>
2023-03-12 18:30:54 +0900

    css 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,23 @@
 import PropTypes from "prop-types";
-  import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({id, medium_cover_image, title, summary, genres}){
-    return <div>
-    <img src={medium_cover_image} alt={title}/>
-    <h2>
-      <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-    <p>{summary}</p>
-    <ul>
-      {genres.map(g => <li key={g}>{g}</li>)}
-    </ul>
-</div>;
+function Movie({ id, coverImg, title, year, summary, genres }) {
+  return (
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-Movie.propTypes = {
-    id: PropTypes.string.isRequired,
-    medium_cover_image: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
-export default Movie;
